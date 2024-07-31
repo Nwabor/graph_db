f72290fb5fffa0c6a470b10644248e149c5d99de
Nwabor <knwabor@gmail.com>
2024-07-31 10:34:30 +0100

    first_migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+zed = Person.create!(first_name: "Zed", last_name: "Flintstone", gender: 'male')
+jed = Person.create!(first_name: "Jed", last_name: "Flintstone", gender: 'male')
+rock = Person.create!(first_name: "Rockbottom", last_name: "Flintstone", gender: 'male')
